9ec3326646d05703bf75ec0a2630399eee6d48f2
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-05-03 23:03:14 +0300

    Added module function pattern

--- factory_functions.js
+++ factory_functions.js
@@ -39,3 +39,21 @@ const schoolBoys = PublicTransport("Kenya Bus", "Umoina Sacco");
 
 schoolBoys.license();
 schoolBoys.colors();
+
+//Module function pattern
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+        add,
+        sub,
+        mul,
+        div,
+    };
+})();
+
+calculator.add(3, 5); // 8
+calculator.sub(6, 2); // 4
+calculator.mul(14, 5534); // 77476
